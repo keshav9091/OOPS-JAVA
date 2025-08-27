78c4b07157a54740abc02b53de7b03c36f493398
keshav9091 <keshav.mavi.cs28@iilm.edu>
2025-08-27 15:57:41 +0530

    Add files via upload

---
+++ take_input.java
@@ -0,0 +1,10 @@
+// To take input and display it
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        System.out.println("My age is " + age);
+    }
+}
