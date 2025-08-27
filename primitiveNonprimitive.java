bc42aec71364ab056de4513681a8becd9698144d
keshav9091 <keshav.mavi.cs28@iilm.edu>
2025-08-27 15:55:31 +0530

    Add files via upload

---
+++ primitiveNonprimitive.java
@@ -0,0 +1,20 @@
+//Primitive and Non primitive data types with there range
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Minimum byte value: " + Byte.MIN_VALUE);
+        System.out.println("Maximum byte value: "+ Byte.MAX_VALUE);
+        System.out.println("Minimum short value: "+ Short.MIN_VALUE);
+        System.out.println("Maximum short value: " + Short.MAX_VALUE);
+        System.out.println("Minimum int value: " + Integer.MIN_VALUE);
+        System.out.println("Maximum int value: "+ Integer.MAX_VALUE);
+        System.out.println("Minimum long value: "+ Long.MIN_VALUE);
+        System.out.println("Maximum long value: "+ Long.MAX_VALUE);
+        System.out.println("Minimum float value: "+ Float.MIN_VALUE);
+        System.out.println("Maximum float value: "+ Float.MAX_VALUE);
+        System.out.println("Minimum double value: "+ Double.MIN_VALUE);
+        System.out.println("Maximum double value: "+ Double.MAX_VALUE);
+        System.out.println("Minimum char value: "+ (int)Character.MIN_VALUE);
+        System.out.println("Maximum char value: "+ (int) Character.MAX_VALUE);
+    }
+}
