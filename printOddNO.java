2a59ef8e192b1f8002ab37c0b3a218dc2d3b8f2f
keshav9091 <keshav.mavi.cs28@iilm.edu>
2025-08-29 12:09:28 +0530

    Add files via upload

---
+++ printOddNO.java
@@ -0,0 +1,16 @@
+// To print the odd numbers from 1 to n
+
+import java.util.*;
+
+public class printOddNo {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n: ");
+        int n = sc.nextInt();
+        for(int i = 1; i <= n ; i++){
+            if(i % 2 != 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
