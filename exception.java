fd4b3524f87f755705428616752e0f32c0d60b8b
keshav9091 <keshav.mavi.cs28@iilm.edu>
2025-09-19 11:30:12 +0530

    Create exception.java

---
+++ exception.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Demo {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a, b, c;
+
+        System.out.print("First Numby : ");
+        a = sc.nextInt();
+
+        System.out.print("Second Num : ");
+        b = sc.nextInt();
+
+        c = a / b;
+
+        System.out.println("Result = " + c);
+        System.out.println("Some other work");
+    }
+}
